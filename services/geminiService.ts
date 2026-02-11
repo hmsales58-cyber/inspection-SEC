5d5be1ba194228987e41be70d1410057dd00823c
hmsales58-cyber <hmsales58@gmail.com>
2026-02-11 18:54:20 +0400

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import { InspectionItem } from "../types";
 
 export const extractDataFromImage = async (base64Image: string): Promise<any> => {
-  const apiKey = "ضع_مفتاحك_هنا"; 
+  const apiKey = "AIzaSyC_LCzI8tHKRd0_YNIj8H_XwjZSWPR8OTw"; 
   const genAI = new GoogleGenerativeAI(apiKey);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
